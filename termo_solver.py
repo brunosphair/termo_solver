86902447c4dafc3aa8f3ee6541acbc5f46838366
sphair <brunosphair@gmail.com>
2022-08-03 20:31:25 -0300

    change to unidecode words only during txt import

--- termo_solver.py
+++ termo_solver.py
@@ -9,27 +9,27 @@ class Solver:
 
     def delete_letter(self, letter):  # Removes from possible words the words that contain the specified letter
         for word in list(self.possible_words):
-            if letter in unidecode(word):
+            if letter in word:
                 self.possible_words.remove(word)
 
     def include_letter_pos(self, letter, letter_position):  # Removes from possible words the words that not contain the
         # specified letter in the specified position
         for word in list(self.possible_words):
-            if unidecode(word[letter_position - 1]) != unidecode(letter):
+            if word[letter_position - 1] != letter:
                 self.possible_words.remove(word)
 
     def include_letter(self, letter, letter_not_position):  # Removes from possible words the words that not contains
         # the specified letter and, after removes the words that contains the specified letter but in the wrong position
         for word in list(self.possible_words):
-            if letter not in unidecode(word):
+            if letter not in word:
                 self.possible_words.remove(word)
-            if unidecode(word[letter_not_position - 1]) == letter:
+            if word[letter_not_position - 1] == letter:
                 self.possible_words.remove(word)
 
     def number_of_letter(self, letter, occurrences):  # Removes from possible words the words that not contains the
         # right number of occurrences of the specified letter
         for word in list(self.possible_words):
-            if unidecode(word).count(letter) != occurrences:
+            if word.count(letter) != occurrences:
                 self.possible_words.remove(word)
 
     def word_picker(self):  # Returns the word to be tested. The criteria is: the word that has most of the more
@@ -42,9 +42,9 @@ class Solver:
         else:
             for word in self.possible_words:
                 for letter in word:
-                    if unidecode(letter) not in letter_list:
-                        letter_list[unidecode(letter)] = 0
-                    letter_list[unidecode(letter)] += 1
+                    if letter not in letter_list:
+                        letter_list[letter] = 0
+                    letter_list[letter] += 1
 
             for letter in self.right_letters:
                 if letter in letter_list:
@@ -62,9 +62,9 @@ class Solver:
             for word in self.all_possible_words:
                 count = 0
                 for y in range(1, final):
-                    if sorted_letters[(-1) * y] in unidecode(word):
+                    if sorted_letters[(-1) * y] in word:
                         count += 1
-                    if unidecode(word[y - 1]) in self.right_letters:
+                    if word[y - 1] in self.right_letters:
                         count -= 1
                 if count > biggest_count:
                     the_word = word
@@ -72,7 +72,7 @@ class Solver:
 
             return the_word
 
-    def classes_analyse(self, word, classes):  # Receives the tried word and a list ('all_results') that
+    def classes_analyse(self, word, classes):  # Receives the tried word and a list ('classes') that
         # contains, in order, the class of each letter of the tried word ('letter wrong', 'letter place' or 'letter
         # right'). Based on this, the function returns a list overwriting 'letter wrong' and 'letter place' classes of
         # letters that appear more than once in the word with -1.
@@ -86,12 +86,12 @@ class Solver:
 
             return right_word
 
-        for letter in set(unidecode(word)):
-            if unidecode(word).count(letter) > 1:
+        for letter in set(word):
+            if word.count(letter) > 1:
                 all_letter_results = []
                 letter_positions = []
                 for j in range(5):
-                    if unidecode(word[j]) == letter:
+                    if word[j] == letter:
                         all_letter_results.append(classes[j])
                         letter_positions.append(j)
                 count_right = all_letter_results.count('letter place') + all_letter_results.count('letter right')
@@ -109,12 +109,12 @@ class Solver:
             result = classes[n]
             if result != -1:
                 if result == 'letter wrong':
-                    self.delete_letter(unidecode(word[n]))
+                    self.delete_letter(word[n])
                 elif result == 'letter place':
-                    self.include_letter(unidecode(word[n]), n + 1)
+                    self.include_letter(word[n], n + 1)
                     self.right_letters.add(word[n])
                 elif result == 'letter right':
-                    self.include_letter_pos(unidecode(word[n]), n + 1)
+                    self.include_letter_pos(word[n], n + 1)
                     self.right_letters.add(word[n])
                 elif result == 'letter right done':
                     right_word = word
@@ -124,11 +124,10 @@ class Solver:
         return right_word
 
 
-
 def load_txt():  # Loads the txt from lexico pt-br and put the words with 5 letters in a list
     words = []
     with open('palavras.txt', encoding='utf8') as f:
         for item in f:
-            words.append(item.rstrip().upper())  # TODO: add unidecode here
+            words.append(unidecode(item.rstrip().upper()))
 
     return words
